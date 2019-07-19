38f02ed0dfab3731d442d9291cafa35fa542139b
sutee.s <sutee.s@linecorp.com>
2019-07-19 15:40:57 +0700

    fix: fix bug

--- src/handle-message/handle-text.js
+++ src/handle-message/handle-text.js
@@ -3,5 +3,8 @@ import { client as line } from '../sdk/line'
 export default (event) => {
   const { message: { text }, replyToken } = event
   const reply = `${text}:  ฉันว้าเหว่`
-  return line.replyMessage(replyToken, reply)
+  return line.replyMessage(replyToken, {
+    type: 'text',  
+    text: reply
+  })
 }
--- src/handle-message/index.js
+++ src/handle-message/index.js
@@ -2,6 +2,10 @@ import handleText from './handle-text'
 import handleDefault from './handle-default'
 
 export default (event) => {
+  if (event.type !== 'message') {
+    return Promise.resolve(null);
+  }
+
   switch(event.message.type) {
     case 'text':
       return handleText(event)
